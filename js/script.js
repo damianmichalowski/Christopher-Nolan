25eac0000d61b7bd81832203f1117e0717195f01
Damian Michalowski <dowski888@gmail.com>
2022-01-31 16:13:50 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+let button = document.querySelector(".button");
+let themeName = document.querySelector(".themeName");
+let body = document.querySelector(".body")
+
+button.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+
+    if (body.classList.contains("dark")) {
+        themeName.innerText = "Jasny";
+    } else {
+        themeName.innerText = "Ciemny";
+    }
+})
+
+
+let butonBiggerLetters = document.querySelector(".buttonBiggerLetters")
+let biggerLetters = document.querySelector("biggerLetters");
+let all = document.querySelector(".all");
+let sizeName = document.querySelector(".sizeName");
+
+butonBiggerLetters.addEventListener("click", () => {
+    all.classList.toggle("biggerLetters");
+
+    if (all.classList.contains("biggerLetters")) {
+        sizeName.innerText = "Zmniejsz";
+    } else {
+        sizeName.innerText = "Powiększ";
+    }
+})
+
+let buttonFatterLetters = document.querySelector(".buttonFatterLetters");
+let fatnessName = document.querySelector(".fatnessName");
+let fatterLetters = document.querySelector(".fatterLetters");
+
+
+buttonFatterLetters.addEventListener("click", () => {
+    all.classList.toggle("fatterLetters");
+
+    if (all.classList.contains("fatterLetters")) {
+        fatnessName.innerText = "Odchudz";
+    } else {
+        fatnessName.innerText = "Pogrub";
+    }
+})
+
+console.log(button);
